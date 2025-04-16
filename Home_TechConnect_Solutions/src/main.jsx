63ff022a21be24ecf1332c1917dbf8da55a075b7
Eduarda Lima <maduridade@gmail.com>
2025-04-16 08:14:59 -0300

    Reestruturei todo o projeto para alocar material ui

--- Home_TechConnect_Solutions/src/main.jsx
+++ Home_TechConnect_Solutions/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+import SignInSide from './templates/sign-in-side/SignInSide.jsx'
+import Dashboard from './templates/dashboard/Dashboard.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <Dashboard />
   </StrictMode>,
 )
